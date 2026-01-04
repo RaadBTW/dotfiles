7e02ae6da3ae7c5d85f813d73930daabf16d0702
raad <alirezzaraad@gmail.com>
2026-01-04 04:07:28 +0330

    - Added a REST client to nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1063,3 +1063,10 @@ vim.lsp.config.ltex = {
 		},
 	},
 }
+
+-- vim rest
+
+vim.g.vrc_get_default_mapping = 0
+vim.g.vrc_output_buffer_name = "_OUTPUT.json"
+vim.g.vrc_response_default_content_type = "application/json"
+vim.g.vrc_auto_format_resposne_patterns = { json = "jq" }
