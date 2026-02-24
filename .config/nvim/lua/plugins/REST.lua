9c8f35e387a590dc518d57dea9a08ddf945a2a7d
raad <alirezzaraad@gmail.com>
2026-02-24 20:41:07 +0330

    - Added some setting to pretty print

--- .config/nvim/lua/plugins/REST.lua
+++ .config/nvim/lua/plugins/REST.lua
@@ -5,5 +5,14 @@ return {
 		vim.g.vrc_output_buffer_name = "_OUTPUT.json"
 		vim.g.vrc_response_default_content_type = "application/json"
 		vim.g.vrc_auto_format_resposne_patterns = { json = "jq" }
+
+		vim.g.vrc_curl_opts = {
+			["--silent"] = "", -- No progress bar
+			["--show-error"] = "", -- Show errors only
+			["-sS"] = "", -- Silent but show errors
+		}
+
+		-- Ensure syntax highlighting
+		vim.g.vrc_syntax_highlight_response = 1
 	end,
 }
